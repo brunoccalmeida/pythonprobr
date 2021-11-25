af4d59b09a858436fdde1a9d2fbcbb9b4f32be50
brunothetav <brunoccalmeida@hotmail.com>
2021-11-24 18:12:41 -0600

    Adicionada flake8 como dependência

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -9,9 +9,10 @@ def buscar_avatar(usuario):
     :return: str com o link do avatar
     """
 
-    url= f'https://api.github.com/users/{usuario}'
+    url = f'https://api.github.com/users/{usuario}'
     resposta = requests.get(url)
     return resposta.json()['avatar_url']
+
 
 if __name__ == '__main__':
     print(buscar_avatar('brunoccalmeida'))
