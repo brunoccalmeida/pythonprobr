c60cc36da0800093c1216b6edb6de2bd29fe6a4c
brunothetav <brunoccalmeida@hotmail.com>
2021-12-08 14:52:10 -0600

    Lib mock do pytest implementada.

--- libpythonbruno/tests/test_spam/test_github_api.py
+++ libpythonbruno/tests/test_spam/test_github_api.py
@@ -6,17 +6,15 @@ from libpythonbruno import github_api
 
 
 @pytest.fixture
-def avatar_url():
-    url = github_api.buscar_avatar('brunoccalmeida')
+def avatar_url(mocker):
     resposta_mock = Mock()
     url = 'https://avatars.githubusercontent.com/u/45464845?v=4'
     resposta_mock.json.return_value = {'login': 'brunoccalmeida',
                                        'id': 45464845, 'node_id': 'MDQ6VXNlcjQ1NDY0ODQ1',
                                        'avatar_url': url}
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resposta_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpythonbruno.github_api.requests.get')
+    get_mock.return_value = resposta_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
